7d10fc25c7eaa9fdcc8858f83fab131d0b6886fa
Evgenii Erokhov <54211329+Evgenii-Erokhov@users.noreply.github.com>
2020-05-05 23:55:46 +0300

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,57 @@
+package lesson6;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        new Client().start("localhost", 8189);
+    }
+
+    public void start(String host, int port) {
+        Thread inputThread = null;
+        try (Socket socket = new Socket(host, port)) {
+            System.out.println("Клиент запущен.");
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            inputThread = runInputThread(in);
+            runOutputLoop(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputThread != null) inputThread.interrupt();
+        }
+    }
+
+    private void runOutputLoop(DataOutputStream out) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String message = scanner.next();
+            out.writeUTF(message);
+            if (message.equals("/end")) {
+                break;
+            }
+        }
+    }
+
+    private Thread runInputThread(DataInputStream in) {
+        Thread thread = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String message = in.readUTF();
+                    System.out.println("From server: " + message);
+                    if (message.equals("/end")) {
+                        System.exit(0);
+                    }
+                } catch (IOException e) {
+                    System.out.println("Connection was closed");
+                    break;
+                }
+            }
+        });
+        thread.start();
+        return thread;
+    }
+}
